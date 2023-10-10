f637c96e8d07f4b11bccf35fbad180f5586baca5
Cfbcx <570496867@qq.com>
2023-10-10 11:47:36 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,22 +3,22 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
-}
-
-fn compose_me(input: &str) -> String {
+    input.trim().to_string()
+  }
+  
+  fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
-}
-
-fn replace_me(input: &str) -> String {
+    format!("{} world!",input)
+  }
+  
+  fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
-}
+    input.replace("cars", "balloons")
+  }
+  
 
 #[cfg(test)]
 mod tests {
