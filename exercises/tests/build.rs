d8b454cfa9281cac019ae28473280607fe812d10
Cfbcx <570496867@qq.com>
2023-10-14 15:42:43 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -20,6 +20,6 @@ fn main() {
     // testcase return early. Fill in the command to tell
     // Cargo about that.
     // let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    //println!("cargo:rustc-cfg=feature=\"pass\"");
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 
 }
